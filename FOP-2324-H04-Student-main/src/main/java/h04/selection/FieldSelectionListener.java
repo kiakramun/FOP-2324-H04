e12346e7bb494cd72d170ee640828d0540824fe1
Krazmer <kiakramun@gmail.com>
2023-12-15 19:00:12 +0100

    add to selection package

---
+++ FOP-2324-H04-Student-main/src/main/java/h04/selection/FieldSelectionListener.java
@@ -0,0 +1,6 @@
+package h04.selection;
+
+import fopbot.Field;
+public interface FieldSelectionListener {
+    void onFieldSelection(Field field);
+}
---
+++ FOP-2324-H04-Student-main/src/main/java/h04/selection/MouseFieldSelector.java
@@ -0,0 +1,27 @@
+package h04.selection;
+
+import fopbot.Field;
+import fopbot.FieldClickEvent;
+import fopbot.FieldClickListener;
+import fopbot.World;
+
+public class MouseFieldSelector implements FieldSelector, FieldClickListener {
+    private Field lastField;
+
+    private FieldSelectionListener listener;
+
+    public MouseFieldSelector(){
+        World.addFieldClickListener(this);
+    }
+
+    public void setFieldSelectionListener(FieldSelectionListener listener) {
+        this.listener = listener;
+    }
+    public void onFieldClick(FieldClickEvent fieldClickEvent) {
+        Field field = fieldClickEvent.getField();
+        if (lastField == field) {
+            listener.onFieldSelection(field);
+        }
+        lastField = field;
+    }
+}
