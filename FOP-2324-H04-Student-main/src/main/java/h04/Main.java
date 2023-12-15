429b911f72dc0cf12d658340e978696885bab8fb
Krazmer <kiakramun@gmail.com>
2023-12-15 07:40:44 +0100

    first commit

---
+++ FOP-2324-H04-Student-main/src/main/java/h04/Main.java
@@ -0,0 +1,29 @@
+package h04;
+
+import fopbot.World;
+
+import static org.tudalgo.algoutils.student.Student.crash;
+
+/**
+ * Main entry point in executing the program.
+ */
+public class Main {
+    /**
+     * Main entry point in executing the program.
+     *
+     * @param args program arguments, currently ignored
+     */
+    public static void main(String[] args) {
+        World.setVisible(true);
+//        main01();
+//        main02();
+    }
+
+    public static void main01() {
+        crash();
+    }
+
+    public static void main02() {
+        crash();
+    }
+}
